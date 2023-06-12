c880c12eae89654c09b849d0022dc6862b1ba64c
Ivan G <mr.zero@inbox.ru>
2023-06-12 17:24:47 +0300

    fix: add proxi for news api

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://rss-news-api.onrender.com/', {
       apiKey: 'eef9fc46616347dbbfb3e24da3a43690', // получите свой ключ https://newsapi.org/
     });
   }
